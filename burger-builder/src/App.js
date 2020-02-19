7db2279b481471594d185b5a90ff18695afb2c2b
Vikash Kumar Singh <VikashS1@verifone.com>
2020-02-19 12:00:01 +0530

    Added css and created component for burger builder

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render(){
     return(
       <div>
         <Layout>
           <h1>My Burger!!!</h1>
+          <BurgerBuilder />
         </Layout>
       </div>
     )
--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Aux from '../../hoc/Auxiliary';
+import './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, sidebar, backdrops</div>
-        <main>
+        <main className="MainContent">
             {props.children}
         </main>
     </Aux>
